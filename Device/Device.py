cd455f4281e0650ad2a6a4c1f9c5a8f972d3544b
Tianjiao Huang <maitian519@gmail.com>
2018-02-14 21:12:12 -0800

    finish up Device.play()

--- Device/Device.py
+++ Device/Device.py
@@ -1,17 +1,40 @@
 class Device():
     
-    def __init__(self):
-        self.pins = []
-        self.is_playing = {k: False for k in keys}
+    def __init__(self, keys, sounds, numpin):
+        self.pins = [False] * numpin
+        self.isPlaying = list(self.pins)
+        self.keys = keys
+        self.sounds = sounds
 
-    def update(self):
+    def updateMapping(self, keys):
+        self
+
+    def updatePins(self):
         raise NotImplementedError
 
     def pressed(self, pin):
         return bool(self.pins[pin])
 
     def play(self):
-        pass
+        self.updatePins()
+        # for all pins that in the keys
+        for i in len(self.pins):
+            pin = self.pins[i]
+            if pin in self.keys:
+                # if pin is on and it is not playing
+                # play the sound and set isPlaying to True
+                if pin and (not self.isPlaying[pin]):
+                    self.sounds[self.keys[pin]].play()
+                    self.isPlaying[pin] = True
+
+                # if pin is not on and it is playing
+                # fade the sound out in 100ms and set isPlaying to False
+                elif (not pin) and self.isPlaying[pin]:
+                    self.sounds[self.keys[pin]].fadeout(100)
+                    self.isPlaying[pin] = False
+
+
+                
 
     
 
